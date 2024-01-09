ce9ab64e3787a1f8d4d8dffa183d1b6496ab042b
Pavel Zakharov <p.a.zakharov@gmail.com>
2024-01-09 21:59:28 +0800

    Add base functions to component

---
+++ readme.md
@@ -0,0 +1,45 @@
+# Youtube Screaper
+The Youtube Screaper is a PHP package that allows you to scrape captions and subtitles from Youtube videos. It uses the Yii 2 framework and the Guzzle HTTP client to make requests to the Youtube API.
+
+### Installation
+You can install the Youtube Screaper using Composer:
+```bash
+composer require zakharov/yii2-youtube-screaper
+```
+### Usage
+To use the Youtube Screaper, you first need to create an instance of the YoutubeScreaper class and set the languageCode property to the language code of the captions you want to scrape. You can then use the getCaptionsBaseUrl and getSubtitles methods to retrieve the base URL and subtitles, respectively.
+
+Here's an example usage:
+```php
+use Zakharov\YoutubeScreaper;
+
+ $screaper = Yii::createObject([
+            'class' => YoutubeScreaper::class,
+        ]);
+        $captionUrl = $screaper->getCaptionsBaseUrl('https://www.youtube.com/watch?v=wNzql5TZ-iY');
+        $subtitles = $screaper->getSubtitles($captionUrl);
+```
+In this example, we're scraping the English captions for the video with the ID wNzql5TZ-iY. The getCaptionsBaseUrl method returns the base URL for the captions, which we then pass to the getSubtitles method to retrieve the subtitles.
+
+Note that the getSubtitles method returns an array of strings, where each string represents a subtitle. You can then use this array to display the subtitles in your application.
+
+For use proxy use .env var HTTPCLIENT_PROXY=http://login:password@domain:port or you can configure and provide own instance of Client to YoutubeScreaper class.
+
+### Testing and Contributing
+The Youtube Screaper includes a test suite to ensure that it works correctly. You can run the tests using the phpunit command:
+```bash
+vendor/bin/codecept run unit
+```
+This will run the test suite and output the results.
+
+The Youtube Screaper follows the PSR-2 coding standards. You can check the code for compliance using the phpcs command:
+
+```bash
+composer check-code
+```
+Fix code by phpcbf
+```bash
+composer fix-code
+```
+License
+The Youtube Screaper is licensed under the MIT license. You can use it free of charge and without any restrictions.
--- src/YoutubeScreaper.php
+++ src/YoutubeScreaper.php
@@ -2,17 +2,136 @@
 
 namespace Zakharov;
 
+use Exception;
+use yii\di\Instance;
 use yii\base\Component;
+use yii\httpclient\Client;
+use yii\httpclient\CurlTransport;
 
 class YoutubeScreaper extends Component
 {
     /**
-     * @param string $videoId
+     * filter captions by language
+     * @var string
+     */
+    public $languageCode = 'en';
+    /**
+     * @var array|Client
+     */
+    public $client = [];
+    /**
+     * @return void
+     */
+    public function init()
+    {
+        parent::init();
+        if (empty($this->client)) {
+            $this->client = $this->getDefaultClientConfig();
+        }
+        $this->client = Instance::ensure($this->client, Client::class);
+    }
+
+    /**
+     * @return array
+     */
+    public function getDefaultClientConfig()
+    {
+        return [
+            'class' => Client::class,
+            'transport' => CurlTransport::class,
+            'requestConfig' => [
+                'headers' => [
+                    'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                    'accept-language' => 'en-En,en;q=0.9',
+                    'cache-control' => 'no-cache',
+                    'pragma' => 'no-cache',
+                    'sec-ch-ua' => '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+                    'sec-ch-ua-mobile' => '?0',
+                    'sec-ch-ua-platform' => '"Windows"',
+                    'sec-fetch-dest:' => 'document',
+                    'sec-fetch-mode' => 'navigate',
+                    'sec-fetch-site' => 'none',
+                    'sec-fetch-user' => '?1',
+                    'upgrade-insecure-requests' => '1',
+                    'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+                ],
+                'options' => [
+                    'proxy' => env('HTTPCLIENT_PROXY'),
+                    'timeout' => 20,
+                    'followLocation' => true,
+                    'maxRedirects' => 10,
+                    'sslVerifyPeer' => false,
+                    CURLOPT_SSL_VERIFYHOST => false,
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @param string $videoUrl
      *
      * @return string
      */
-    public function getCaptionsBaseUrl(string $videoId)
+    public function getCaptionsBaseUrl(string $videoUrl)
     {
-        return 'https://www.youtube.com/api/timedtext?v=' . $videoId;
+        $request = $this->client->get($videoUrl);
+        $response = $request->send();
+        if (!$response->isOk) {
+            $statusCode = $response->getStatusCode();
+            $responseBody = $response->getContent();
+            throw new Exception("Failed to get captions: $statusCode: $responseBody");
+        }
+        $html = $response->getContent();
+        preg_match('/"captionTracks":([^\]]*])/', $html, $matches);
+        if (empty($matches[1]) || !strpos($matches[1], '"baseUrl":')) {
+            throw new Exception("Url not find: $matches[1]");
+        }
+        $results = json_decode($matches[1], true);
+        $result = array_filter($results, function ($result) {
+            return $result['languageCode'] == $this->languageCode;
+        });
+        if (!count($result)) {
+            throw new Exception("Url not find by languageCode: $matches[1]");
+        }
+        $baseUrl = $result[0]['baseUrl'] ?? null;
+        return $baseUrl;
+    }
+
+    /**
+     * @param string $captionUrl
+     *
+     * @return array
+     */
+    public function getSubtitles(string $captionUrl)
+    {
+        $request = $this->client->get($captionUrl);
+        $response = $request->send();
+        if (!$response->isOk) {
+            $statusCode = $response->getStatusCode();
+            $responseBody = $response->getContent();
+            throw new Exception("Failed to get subs: $statusCode: $responseBody");
+        }
+        $subtitlesContent = $response->getContent();
+        $subtitles = $this->parseXmlTextNodes($subtitlesContent);
+        return $subtitles;
+    }
+
+    /**
+     * Parses an XML string and returns the content of all <text> nodes as an array.
+     *
+     * @param string $xml The XML string to be parsed.
+     * @return array The content of all <text> nodes as an array.
+     */
+    private function parseXmlTextNodes($xml)
+    {
+        $result = [];
+        $xmlObject = simplexml_load_string($xml);
+        if ($xmlObject) {
+            $textNodes = $xmlObject->xpath('//text');
+            foreach ($textNodes as $textNode) {
+                $result[] = (string) $textNode;
+            }
+        }
+        return $result;
     }
 }
